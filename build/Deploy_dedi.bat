271e651070b284214d1780b0cfc397ca62089979
Jukki <judejoki@gmail.com>
2015-06-05 00:48:42 +0300

    Fixed PATH variable

--- build/Deploy_dedi.bat
+++ build/Deploy_dedi.bat
@@ -2,13 +2,13 @@ pushd %~dp0
 
 set PWA_PATH="%~1\@EndOfDayZ\addons"
 
-mkdir PWA_PATH
+mkdir %PWA_PATH%
 mkdir .\building
 
 for /D %%f in (.\..\Client\*) do cpbo.exe -p "%%f" "building\%%~nxf.pbo"
 
 for %%F in (building\*.pbo) do DSSignFile "c:\endofdayz.biprivatekey" %%F
 
-xcopy /s/e/y .\building\* PWA_PATH
+xcopy /s/e/y .\building\* %PWA_PATH%
 
 popd
