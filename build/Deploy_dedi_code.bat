854b648066487083c33553bfb90debcd668ea4ed
Jukki <judejoki@gmail.com>
2015-06-05 13:48:51 +0300

    Made sure also bising is moved when building just code

--- build/Deploy_dedi_code.bat
+++ build/Deploy_dedi_code.bat
@@ -9,7 +9,7 @@ cpbo.exe -p .\..\Client\dayz_code ".\building\dayz_code.pbo"
 
 DSSignFile "c:\endofdayz.biprivatekey" .\building\dayz_code.pbo
 
-move /y .\building\dayz_code.pbo %PWA_PATH%
+move /y .\building\dayz_code.* %PWA_PATH%
 
 rmdir /s/q .\building
 
