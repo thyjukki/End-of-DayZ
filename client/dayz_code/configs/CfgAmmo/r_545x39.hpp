f7653061f1931cacaf7942a36a76e7771adfb537
Jukki <judejoki@gmail.com>
2015-06-06 13:47:41 +0300

    Fixed small bugs with  545x39 ammo

--- client/dayz_code/configs/CfgAmmo/r_545x39.hpp
+++ client/dayz_code/configs/CfgAmmo/r_545x39.hpp
@@ -1,13 +1,20 @@
-class B_545x39_Ball;
-
-class B_545x39_Ball_HP: B_545x39_Ball
+class B_545x39_Ball : BulletBase
+{
+	model="";
+	tracerScale=0;
+	tracerStartTime=0;
+	tracerEndTime=0;
+	nvgOnly=0;
+};
+				
+class B_545x39_HP: B_545x39_Ball
 {
 	hit=10;
 	airFriction=-0.0006;
 	typicalSpeed=320;
 };
 
-class B_545x39_Ball_TRACER: B_545x39_Ball
+class B_545x39_TRACER: B_545x39_Ball
 {
 	model = "\ca\Weapons\Data\bullettracer\tracer_red";
 	tracerendtime = 1;
@@ -15,7 +22,7 @@ class B_545x39_Ball_TRACER: B_545x39_Ball
 	tracerstarttime = 0.05;
 };
 
-class B_545x39_Ball_RUBBER: B_545x39_Ball
+class B_545x39_RUBBER: B_545x39_Ball
 {
 	airfriction = -0.01;
 	caliber = 0.1;
@@ -24,7 +31,7 @@ class B_545x39_Ball_RUBBER: B_545x39_Ball
 	typicalSpeed=150;
 };
 
-class B_545x39_Ball_BT: B_545x39_Ball
+class B_545x39_BT: B_545x39_Ball
 {
 	hit=10;
 };
