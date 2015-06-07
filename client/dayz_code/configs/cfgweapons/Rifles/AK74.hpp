640a6881318b0aa9a6fe919c4060beba14380127
Jukki <judejoki@gmail.com>
2015-06-07 22:09:31 +0300

    Fixed small error with ak_base

--- client/dayz_code/configs/cfgweapons/Rifles/AK74.hpp
+++ client/dayz_code/configs/cfgweapons/Rifles/AK74.hpp
@@ -1,6 +1,31 @@
 /* Kobra */
 
-class AK_BASE;
+class AK_BASE: Rifle
+{
+	magazines[] = 
+	{
+		"30Rnd_762x39_AK47",
+		"30Rnd_762x39_AK47_SD",
+		"30Rnd_762x39_AK47_hp",
+		"30Rnd_762x39_AK47_tracer",
+		"30Rnd_762x39_AK47_rubber",
+		"30Rnd_762x39_AK47_bt",
+		"vil_40Rnd_762x39_AK47",
+		"vil_40Rnd_762x39_AK47_SD",
+		"vil_40Rnd_762x39_AK47_hp",
+		"vil_40Rnd_762x39_AK47_tracer",
+		"vil_40Rnd_762x39_AK47_rubber",
+		"vil_40Rnd_762x39_AK47_bt",
+		"RH_75Rnd_762x39_mag",
+		"RH_75Rnd_762x39_mag_SD",
+		"RH_75Rnd_762x39_mag_hp",
+		"RH_75Rnd_762x39_mag_tracer",
+		"RH_75Rnd_762x39_mag_rubber",
+		"RH_75Rnd_762x39_mag_bt"
+	};
+};
+
+
 class AK_74 : AK_BASE
 {
 	magazines[]=
--- client/dayz_code/configs/cfgweapons/Rifles/AKM.hpp
+++ client/dayz_code/configs/cfgweapons/Rifles/AKM.hpp
@@ -1,27 +1,4 @@
-class AK_BASE: Rifle
-{
-	magazines[] = 
-	{
-		"30Rnd_762x39_AK47",
-		"30Rnd_762x39_AK47_SD",
-		"30Rnd_762x39_AK47_hp",
-		"30Rnd_762x39_AK47_tracer",
-		"30Rnd_762x39_AK47_rubber",
-		"30Rnd_762x39_AK47_bt",
-		"vil_40Rnd_762x39_AK47",
-		"vil_40Rnd_762x39_AK47_SD",
-		"vil_40Rnd_762x39_AK47_hp",
-		"vil_40Rnd_762x39_AK47_tracer",
-		"vil_40Rnd_762x39_AK47_rubber",
-		"vil_40Rnd_762x39_AK47_bt",
-		"RH_75Rnd_762x39_mag",
-		"RH_75Rnd_762x39_mag_SD",
-		"RH_75Rnd_762x39_mag_hp",
-		"RH_75Rnd_762x39_mag_tracer",
-		"RH_75Rnd_762x39_mag_rubber",
-		"RH_75Rnd_762x39_mag_bt"
-	};
-};
+
 
 class AKM_DZ : AK_47_M
 {
