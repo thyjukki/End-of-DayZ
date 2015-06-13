c06e03cc0c77964204f39f3ed6cee917425b163c
Jukki <judejoki@gmail.com>
2015-06-13 23:52:04 +0300

    Fixed few lefovers

--- client/rh_m4_cfg/config.cpp
+++ client/rh_m4_cfg/config.cpp
@@ -171,6 +171,12 @@ class CfgWeapons
 		};
 		magazines[] =
 		{
+			"556x45_x1",
+			"556x45_SD_x1",
+			"556x45_HP_x1",
+			"556x45_TRACER_x1",
+			"556x45_RUBBER_x1",
+			"556x45_BT_x1",
 			"20Rnd_556x45_Stanag",
 			"20Rnd_556x45_StanagSD",
 			"20Rnd_556x45_Stanag_hp",
--- client/ukf_ukweps_oa_cfg/config.cpp
+++ client/ukf_ukweps_oa_cfg/config.cpp
@@ -103,6 +103,12 @@ class CfgWeapons
 	class L85A2_base_BAF: Rifle
 	{
 		magazines[] = {
+			"556x45_x1",
+			"556x45_SD_x1",
+			"556x45_HP_x1",
+			"556x45_TRACER_x1",
+			"556x45_RUBBER_x1",
+			"556x45_BT_x1",
 			"20Rnd_556x45_Stanag",
 			"20Rnd_556x45_StanagSD",
 			"20Rnd_556x45_Stanag_hp",
