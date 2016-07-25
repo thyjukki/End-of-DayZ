4eafa3127d02cb359d6a73a9d9ab77a3a7078e14
Jussi Joki <judejoki@gmail.com>
2016-07-25 21:40:36 +0300

    updated client deploy script

--- arma3/build/Deploy_client.bat
+++ arma3/build/Deploy_client.bat
@@ -1,14 +1,16 @@
 pushd %~dp0
-set PWA_PATH="%~1\addons"
 
-rmdir  /s/q .\@EndOFDayz
+set PWA_PATH="%~1\@DayzAwaken\addons"
 
 mkdir %PWA_PATH%
-mkdir .\@EndOFDayz\addons
+mkdir .\building
 
-for /D %%f in (.\..\Client\*) do cpbo.exe -p "%%f" ".\@EndOFDayz\addons\%%~nxf.pbo"
+for /D %%f in (.\..\addons\*) do cpbo.exe -p "%%f" "building\%%~nxf.pbo"
 
-for %%F in (.\@EndOFDayz\addons\*.pbo) do DSSignFile "c:\endofdayz.biprivatekey" %%F
+for %%F in (building\*.pbo) do DSSignFile "c:\endofdayz.biprivatekey" %%F
 
-move /y .\@EndOFDayz\addons\* %PWA_PATH%
+move /y .\building\* %PWA_PATH%
+
+rmdir /s/q .\building
+
 popd
