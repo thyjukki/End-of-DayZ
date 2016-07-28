a91b3fe00fce2ff411391fe68120f48d2f0ad7e8
Jussi Joki <jussi.joki@futuremark.com>
2016-07-28 13:13:20 +0300

    Started working on auto updater

---
+++ DAUpdater/DAUpdater/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DAUpdater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //add some bootstrap or startup logic 
+            if (DAUpdater.Properties.Settings.Default.FirstTime)
+            {
+                Settings settings = new Settings();
+                settings.Show();
+            }
+            else
+            {
+                MainWindow mainView = new MainWindow();
+                mainView.Show();
+            }
+        }
+    }
+}
---
+++ DAUpdater/DAUpdater/Settings.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DAUpdater
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            arma2PathBox.Text = Properties.Settings.Default.ArmaPath;
+            modPathBox.Text = Properties.Settings.Default.ModPath;
+            parametersBox.Text = Properties.Settings.Default.Paremeters;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.ArmaPath = arma2PathBox.Text;
+            Properties.Settings.Default.ModPath = modPathBox.Text;
+            Properties.Settings.Default.Paremeters = parametersBox.Text;
+            DAUpdater.Properties.Settings.Default.FirstTime = false;
+            DAUpdater.Properties.Settings.Default.Save();
+            bool windowed;
+            if(windowedBox.IsChecked.HasValue)
+            {
+                windowed = windowedBox.IsChecked.Value;
+            }
+            else
+            {
+                windowed = false;
+            }
+            Properties.Settings.Default.Windowed = windowed;
+        }
+    }
+}
