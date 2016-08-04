a182a5743327d996fa2e642208f8062d4593d552
thyjukki <judejoki@gmail.com>
2016-08-04 22:42:42 +0300

    more fixing to loot tables

--- client/dayz_code/configs/CfgLoot/Groups/Attachments.hpp
+++ client/dayz_code/configs/CfgLoot/Groups/Attachments.hpp
@@ -9,19 +9,19 @@ AttachmentsGeneric[] =
 */
 AttachmentsEast[] =
 {
-	{Loot_MAGAZINE,		05 		Attachment_PSO1},
-	{Loot_MAGAZINE,		10		Attachment_Kobra},
-	{Loot_MAGAZINE,		05		Attachment_GP25},
-	{Loot_MAGAZINE,		03		Attachment_Sup},
+	{Loot_MAGAZINE,		05, 		Attachment_PSO1},
+	{Loot_MAGAZINE,		10,		Attachment_Kobra},
+	{Loot_MAGAZINE,		05,		Attachment_GP25},
+	{Loot_MAGAZINE,		03,		Attachment_Sup},
 };
 
 AttachmentsWest[] =
 {
-	{Loot_MAGAZINE,		03 		Attachment_Sup},
-	{Loot_MAGAZINE,		15 		Attachment_CCO},
-	{Loot_MAGAZINE,		15 		Attachment_Holo},
-	{Loot_MAGAZINE,		10 		Attachment_M203},
-	{Loot_MAGAZINE,		05 		Attachment_ACOG},
-	{Loot_MAGAZINE,		02 		Attachment_SCOPED},
-	{Loot_MAGAZINE,		0.1 		Attachment_Sup308},
+	{Loot_MAGAZINE,		03,		Attachment_Sup},
+	{Loot_MAGAZINE,		15,		Attachment_CCO},
+	{Loot_MAGAZINE,		15, 		Attachment_Holo},
+	{Loot_MAGAZINE,		10,		Attachment_M203},
+	{Loot_MAGAZINE,		05, 		Attachment_ACOG},
+	{Loot_MAGAZINE,		02, 		Attachment_SCOPED},
+	{Loot_MAGAZINE,		0.1,		Attachment_Sup308},
 };
